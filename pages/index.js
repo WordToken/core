fbd8035a76970aad0c0d4ed5edb255091480b6e5
Lemuel Uhuru <wish@devgenie.com>
2020-05-13 18:18:06 -0400

    fix import typos

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
 
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 
 
 const Index = props => {
